c371cf9d19a8929022d7663e2ad202b4784d17ea
Murilo Gomes <murilogomes@pop-os.localdomain>
2021-02-12 12:14:48 -0300

    FEAT: connect to github api to get list of repos

--- projetossimilares/src/App.js
+++ projetossimilares/src/App.js
@@ -1,14 +1,19 @@
-import {useEffect} from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import cytoscape from 'cytoscape';
+//import api from './services/api';
+import axios from 'axios';
 
 function App() {
 
-  useEffect(()=>{
+  const [repoCount, setRepoCount] = useState(0);
+  let repos = [];
+
+  useEffect(() => {
     var cy = cytoscape({
 
       container: document.getElementById('cy'), // container to render in
-  
+
       elements: [ // list of graph elements to start with
         { // node a
           data: { id: 'a' }
@@ -20,7 +25,7 @@ function App() {
           data: { id: 'ab', source: 'a', target: 'b' }
         }
       ],
-  
+
       style: [ // the stylesheet for the graph
         {
           selector: 'node',
@@ -29,7 +34,7 @@ function App() {
             'label': 'data(id)'
           }
         },
-  
+
         {
           selector: 'edge',
           style: {
@@ -41,15 +46,51 @@ function App() {
           }
         }
       ],
-  
+
       layout: {
         name: 'grid',
         rows: 1
       }
-  
+
     });
   }, []);
 
+  useEffect(async () => {
+    let response = await axios.get('https://api.github.com/orgs/projeto-de-algoritmos/repos?per_page=100');
+    let {next} = parseData(response.headers.link);
+    repos = repos.concat(response.data);
+    while(next){
+      response = await axios.get(next);
+      next = parseData(response.headers.link).next;
+      repos = repos.concat(response.data);
+    }
+    console.log(repos);    
+  }, []);
+
+  function parseData(data) {
+    let arrData = data.split("link:")
+    data = arrData.length == 2? arrData[1]: data;
+    let parsed_data = {}
+
+    arrData = data.split(",")
+
+    for (var d of arrData){
+        var linkInfo = /<([^>]+)>;\s+rel="([^"]+)"/ig.exec(d)
+
+        parsed_data[linkInfo[2]]=linkInfo[1]
+    }
+
+    return parsed_data;
+}
+
+  /*useEffect(() => {
+    api.get(`repos?per_page=100&page=${(repoCount / 100)}`).then((response2) => {
+      setRepos(...repos, response2.data);
+    });
+    setRepoCount(repoCount-100);
+    console.log(repos);
+  }, [repoCount])*/
+  
 
   return (
     <div className="App">
