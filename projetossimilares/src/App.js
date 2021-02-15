8df019ccf46a3345de1ad7b2f20a55400efead9d
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-02-15 14:14:59 -0300

    REFACTOR: Separate function that renders graph into a new file

--- projetossimilares/src/App.js
+++ projetossimilares/src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import cytoscape from 'cytoscape';
-//import api from './services/api';
+import renderGraph from './utils/renderGraph';
 import axios from 'axios';
 
 function App() {
@@ -10,49 +9,7 @@ function App() {
   let repos = [];
 
   useEffect(() => {
-    var cy = cytoscape({
-
-      container: document.getElementById('cy'), // container to render in
-
-      elements: [ // list of graph elements to start with
-        { // node a
-          data: { id: 'a' }
-        },
-        { // node b
-          data: { id: 'b' }
-        },
-        { // edge ab
-          data: { id: 'ab', source: 'a', target: 'b' }
-        }
-      ],
-
-      style: [ // the stylesheet for the graph
-        {
-          selector: 'node',
-          style: {
-            'background-color': '#666',
-            'label': 'data(id)'
-          }
-        },
-
-        {
-          selector: 'edge',
-          style: {
-            'width': 3,
-            'line-color': '#ccc',
-            'target-arrow-color': '#ccc',
-            'target-arrow-shape': 'none',
-            'curve-style': 'haystack, demo'
-          }
-        }
-      ],
-
-      layout: {
-        name: 'grid',
-        rows: 1
-      }
-
-    });
+    renderGraph();
   }, []);
 
   useEffect(async () => {
